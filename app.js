4e8f81227f6c9996a77bb67be04c78d355ebf12e
BahlulBoy <bahlulsukses@gmail.com>
2022-11-30 11:42:25 +0700

    --

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
-const expressEjsLayouts = require('express-ejs-layouts');
 const app = express();
+const layout = require("express-ejs-layouts");
 const port = 5000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(expressEjsLayouts);
 
 app.get('/', (req, res) => {
     res.render('index');
 })
-
 
 app.use('/', (req, res) => {
     res.send("Halaman Tidak Ditemukan");
