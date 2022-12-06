8afa2a61d6b3dab1241cedb60af728d240d6e093
BahlulBoy <bahlulsukses@gmail.com>
2022-12-06 14:44:44 +0700

    --

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get('/profile', (req, res) => {
     res.render('profile');
 })
 app.get('/anggota', (req, res) => {
-    con.connect('anggota', res, 'SELECT *, nama_periode FROM anggota INNER JOIN periode ON anggota.periode = periode.id_periode');
+    con.connect('anggota', res, 'SELECT *, tahun FROM anggota INNER JOIN periode ON anggota.periode = periode.id_periode');
 })
 app.get('/berita', (req, res) => {
     con.connect('berita', res, 'SELECT *, nama FROM berita INNER JOIN anggota ON berita.penulis = anggota.nim');
@@ -61,6 +61,13 @@ app.get('/add', (req, res) => {
             res.render("./add_page/periode", {layout: "./layout/errorlayout"});
             break
         case "proker":
+            var data = pool.getConnection((err, connection) => {
+                connection.query("SELECT nama_periode FROM periode", function (err, result, field) {
+                  if (err) throw err;
+                  res.render("./add_page/anggota", {database : result, layout: "./layout/errorlayout"});
+                  connection.release();
+                })
+            })
             res.render("./add_page/proker", {layout: "./layout/errorlayout"});
             break
         case "user":
@@ -72,6 +79,32 @@ app.get('/add', (req, res) => {
 })
 
 app.post('/add_berita', urlencodedParser, (req, res) => {
+    var judul_berita = req.body.judul_berita;
+    var isi_berita = req.body.isi_berita;
+    var penulis = req.body.penulis;
+    var data = pool.getConnection((err, connection) => {
+        connection.query("INSERT INTO `berita` (`judul_berita`, `isi_berita`, `penulis`) VALUES " + "("  + "'"+ judul_berita +"'" + ", "  + "'"+ isi_berita +"'" + ", " + "(SELECT nim FROM anggota WHERE nama="+ "'" + penulis + "'" + ")" +")" , function (err, result, field) {      
+            if (err) throw err;      
+            res.redirect("/berita");
+            connection.release();    
+        })
+    })
+})
+
+app.post('/add_anggota', urlencodedParser, (req, res) => {
+    var nim = (req.body.nim).toString();
+    var nama = (req.body.nama).toString();
+    var tanggal_lahir = (req.body.tanggal_lahir);
+    var prodi = (req.body.prodi).toString();
+    var periode = (req.body.periode).toString();
+    var jabatan = (req.body.jabatan).toString();
+    var data = pool.getConnection((err, connection) => {
+        connection.query("INSERT INTO `anggota` (`nim`, `nama`, `tanggal_lahir`, `prodi`, `periode`, `jabatan`) VALUES " + "("  + "'"+ nim +"'" + ", " + "'"+ nama +"'" + ", " + "'"+ tanggal_lahir +"'" + ", " + "'"+ prodi +"'" + ", " + "(SELECT id_periode FROM periode WHERE nama_periode="+ "'" + periode + "'" + ")" + ", " + "'"+ jabatan +"'" +")" , function (err, result, field) {      
+            if (err) throw err;      
+            res.redirect("/anggota");
+            connection.release();    
+        })
+    })
 })
 
 app.post('/add_user', urlencodedParser, (req, res) => {
@@ -90,6 +123,33 @@ app.post('/add_user', urlencodedParser, (req, res) => {
     })
 })
 
+app.post('/add_periode', urlencodedParser, (req, res) => {
+    var tahun = req.body.tahun
+    var nama_periode = req.body.nama_periode
+    var data = pool.getConnection((err, connection) => {
+        connection.query("INSERT INTO `periode` (`tahun`, `nama_periode`) VALUES " + "("  + "'"+ tahun +"'" + ", " + "'"+ nama_periode  +"'" +")", function (err, result, field) {      
+            if (err) throw err;      
+            res.redirect("/periode");
+            connection.release();    
+        })
+    })
+})
+
+app.post('/add_proker', urlencodedParser, (req, res) => {
+    var nim = (req.body.nim).toString();
+    var nama = (req.body.nama).toString();
+    var tanggal_lahir = (req.body.tanggal_lahir);
+    var prodi = (req.body.prodi).toString();
+    var periode = (req.body.periode).toString();
+    var jabatan = (req.body.jabatan).toString();
+    var data = pool.getConnection((err, connection) => {
+        connection.query("INSERT INTO `anggota` (`nim`, `nama`, `tanggal_lahir`, `prodi`, `periode`, `jabatan`) VALUES " + "("  + "'"+ nim +"'" + ", " + "'"+ nama +"'" + ", " + "'"+ tanggal_lahir +"'" + ", " + "'"+ prodi +"'" + ", " + "(SELECT id_periode FROM periode WHERE nama_periode="+ "'" + periode + "'" + ")" + ", " + "'"+ jabatan +"'" +")" , function (err, result, field) {      
+            if (err) throw err;      
+            res.redirect("/anggota");
+            connection.release();    
+        })
+    })
+})
 
 app.use('/', (req, res) => {
     res.render('error', { layout : "./layout/errorlayout" });
